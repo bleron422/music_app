dedcd00765a5da390201ee192a233a3652a6cf6a
_bleron__ <bmurseli57@gmail.com>
2021-04-07 20:47:43 +0200

    update

---
+++ src/util.js
@@ -0,0 +1,88 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chillHop() {
+  return [
+    {
+      name: "Beaver Creek",
+      cover:
+        "https://d34qmkt8w5wll9.cloudfront.net/commercial-releases/cover_art/jpeg/5777.jpg",
+      artist: "Aso , Middle School, Aviino",
+      audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Many Men",
+      cover:
+        "https://d34qmkt8w5wll9.cloudfront.net/commercial-releases/cover_art/jpeg/3349.jpg",
+      artist: "50 Cent",
+      audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Beautiful",
+      cover:
+        "https://d34qmkt8w5wll9.cloudfront.net/commercial-releases/cover_art/jpeg/5668.jpg",
+      artist: "Eminem",
+      audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Beautiful",
+      cover:
+        "https://d34qmkt8w5wll9.cloudfront.net/commercial-releases/cover_art/jpeg/5668.jpg",
+      artist: "Eminem",
+      audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Many Men",
+      cover:
+        "https://d34qmkt8w5wll9.cloudfront.net/commercial-releases/cover_art/jpeg/3349.jpg",
+      artist: "50 Cent",
+      audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Many Men",
+      cover:
+        "https://d34qmkt8w5wll9.cloudfront.net/commercial-releases/cover_art/jpeg/3349.jpg",
+      artist: "50 Cent",
+      audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Many Men",
+      cover:
+        "https://d34qmkt8w5wll9.cloudfront.net/commercial-releases/cover_art/jpeg/3349.jpg",
+      artist: "50 Cent",
+      audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Many Men",
+      cover:
+        "https://d34qmkt8w5wll9.cloudfront.net/commercial-releases/cover_art/jpeg/3349.jpg",
+      artist: "50 Cent",
+      audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+  ];
+}
+
+export default chillHop;
